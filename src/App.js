34bbff502b4f1d4dc5e0211d7b8811422d55da35
jd228u <101908876+jd228u@users.noreply.github.com>
2025-03-31 18:56:10 +1300

    Initial project setup
    - React scaffold
    - README

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HairPix</h1>
+      <p>See yourself with a fresh new look!</p>
+    </div>
+  );
+}
+export default App;
